236a1d5a370a869221820ade6409b3245509da24
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2024-01-10 14:33:34 +0300

    Email,Password and Name Control Methods Added

--- app/src/main/java/com/yusuf/cryptocurrencytrading/utils/Utils.kt
+++ app/src/main/java/com/yusuf/cryptocurrencytrading/utils/Utils.kt
@@ -1,7 +1,37 @@
 package com.yusuf.cryptocurrencytrading.utils
 
+import android.content.Context
 import android.view.View
+import android.widget.Toast
 
+class Utils{
+    companion object{
+        const val emailRegex = "^[\\w.-]+@[\\w.-]+\\.[a-zA-Z]{2,}$"
+        const val nameRegex = "^[a-zA-ZğüşıöçĞÜŞİÖÇ]+$"
+
+        fun  emailAndPasswordControl(email:String,password:String,context: Context) :Boolean{
+            return if (email.equals("") || password.equals("")) {
+                Toast.makeText(context, "E-mail or Password can not be empty.", Toast.LENGTH_LONG).show();
+                false
+            } else if (!email.matches(emailRegex.toRegex())) {
+                Toast.makeText(context, "Please write your email in the correct format.", Toast.LENGTH_LONG).show();
+                false;
+            } else {
+                true;
+            }
+        }
+
+        fun nameControl(name:String,context: Context):Boolean{
+            return if (name.matches(nameRegex.toRegex())){
+                true;
+            } else {
+                Toast.makeText(context,"Name can not be empty!",Toast.LENGTH_LONG).show();
+                false;
+            }
+        }
+
+    }
+}
 
 fun View.invisible(){
     visibility = View.INVISIBLE
@@ -14,3 +44,4 @@ fun View.visible(){
 fun View.gone(){
     visibility = View.GONE
 }
+
