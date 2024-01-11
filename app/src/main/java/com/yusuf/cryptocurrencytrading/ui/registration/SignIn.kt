73e0c030d3974845382e0de26af5ce63bc4dce58
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2024-01-11 17:36:20 +0300

    Sign In and User Check Methods Completed

--- app/src/main/java/com/yusuf/cryptocurrencytrading/ui/registration/SignIn.kt
+++ app/src/main/java/com/yusuf/cryptocurrencytrading/ui/registration/SignIn.kt
@@ -5,27 +5,57 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
-import com.yusuf.cryptocurrencytrading.R
 import com.yusuf.cryptocurrencytrading.databinding.FragmentSignInBinding
+import com.yusuf.cryptocurrencytrading.ui.registration.viewModel.RegistrationViewModel
 
 class SignIn : Fragment() {
 
     private lateinit var binding: FragmentSignInBinding
+    private lateinit var viewModel: RegistrationViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentSignInBinding.inflate(inflater,container,false)
-        return binding.root
+        viewModel = ViewModelProvider(this).get(RegistrationViewModel::class.java)
 
+        checkUserIfNotNull()
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.goToSingUp.setOnClickListener {
             val action = SignInDirections.actionSignInToSignUp()
+            findNavController().navigate(action)
+        }
+
+        binding.buttonSignIn.setOnClickListener {
+            val email = binding.editTextEmail.text.toString()
+            val password = binding.editTextPassword.text.toString()
+
+            signIn(email,password)
+        }
+
+    }
+
+    private fun signIn(email:String,password:String){
+
+        viewModel.signIn(email,password,requireContext(),{
+            val action = SignInDirections.actionSignInToMainCryptoFragment()
+            findNavController().navigate(action)
+        },{
+            Toast.makeText(requireContext(), it, Toast.LENGTH_LONG).show()
+        })
+    }
+
+    private fun checkUserIfNotNull(){
+        viewModel.checkUserIfNotNull {
+            val action = SignInDirections.actionSignInToMainCryptoFragment()
             findNavController().navigate(action)
         }
     }
--- app/src/main/java/com/yusuf/cryptocurrencytrading/ui/registration/viewModel/RegistrationViewModel.kt
+++ app/src/main/java/com/yusuf/cryptocurrencytrading/ui/registration/viewModel/RegistrationViewModel.kt
@@ -1,27 +1,45 @@
 package com.yusuf.cryptocurrencytrading.ui.registration.viewModel
 
 import android.content.Context
-import android.util.Log
-import android.widget.Toast
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.google.android.gms.tasks.OnFailureListener
-import com.google.android.gms.tasks.OnSuccessListener
-import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.yusuf.cryptocurrencytrading.utils.Utils
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+
 
 class RegistrationViewModel : ViewModel() {
 
-     val auth: FirebaseAuth by lazy { FirebaseAuth.getInstance() }
-     val firestore: FirebaseFirestore by lazy { FirebaseFirestore.getInstance() }
+     var loading = MutableLiveData<Boolean>()
 
-    fun signUp(email: String, password: String, name: String, onSuccess: () -> Unit, onFailure: (String) -> Unit) {
-        if (email.isNotEmpty() && password.isNotEmpty()) {
 
+     private val auth: FirebaseAuth by lazy { FirebaseAuth.getInstance() }
+     private val firestore: FirebaseFirestore by lazy { FirebaseFirestore.getInstance() }
+
+    fun checkUserIfNotNull(action: () -> Unit){
+        val user = auth.currentUser
+
+        if (user != null){
+            action()
+        }
+    }
+
+    fun signIn(email: String, password: String,context: Context,onSuccess: () -> Unit, onFailure: (String) -> Unit){
+        if (Utils.emailAndPasswordControl(email,password,context)){
+           auth.signInWithEmailAndPassword(email,password).addOnSuccessListener {
+               onSuccess()
+           }.addOnFailureListener {
+               onFailure(it.localizedMessage ?: "An unknown error occurred.")
+           }
+        }
+        else {
+            onFailure("E-mail or Password can not be empty.")
+        }
+    }
+
+    fun signUp(email: String, password: String, name: String, context: Context,onSuccess: () -> Unit, onFailure: (String) -> Unit) {
+        loading.value = true
+        if (Utils.nameControl(name,context) && Utils.emailAndPasswordControl(email,password,context)){
             auth.createUserWithEmailAndPassword(email, password)
                 .addOnSuccessListener { authResult ->
                     val user = hashMapOf(
@@ -35,6 +53,7 @@ class RegistrationViewModel : ViewModel() {
                         .set(user)
                         .addOnSuccessListener {
                             onSuccess()
+                            loading.value = false
                         }
                         .addOnFailureListener {
                             onFailure(it.localizedMessage ?: "An unknown error occurred.")
@@ -43,7 +62,8 @@ class RegistrationViewModel : ViewModel() {
                 }.addOnFailureListener {
                     onFailure(it.localizedMessage ?: "An unknown error occurred.")
                 }
-        } else {
+        }
+         else {
             onFailure("E-mail or Password can not be empty.")
         }
     }
