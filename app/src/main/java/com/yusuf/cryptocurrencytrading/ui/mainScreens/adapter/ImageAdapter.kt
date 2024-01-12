bef4c2dc165357877c1e8e8a1e2b6a7a58434fd5
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2024-01-12 16:16:02 +0300

    ViewPager2

---
+++ app/src/main/java/com/yusuf/cryptocurrencytrading/ui/mainScreens/adapter/ImageAdapter.kt
@@ -0,0 +1,30 @@
+package com.yusuf.cryptocurrencytrading.ui.mainScreens.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.yusuf.cryptocurrencytrading.R
+
+class ImageAdapter(private val imageResources: Array<Int>) :
+    RecyclerView.Adapter<ImageAdapter.ImageViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_image, parent, false)
+        return ImageViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
+        holder.imageView.setImageResource(imageResources[position])
+    }
+
+    override fun getItemCount(): Int {
+        return imageResources.size
+    }
+
+    inner class ImageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imageView: ImageView = itemView.findViewById(R.id.imageView)
+    }
+}
