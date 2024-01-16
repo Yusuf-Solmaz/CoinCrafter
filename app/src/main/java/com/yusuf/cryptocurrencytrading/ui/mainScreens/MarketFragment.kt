dc3588b4a589b9ec621f9898bc9184157e41d744
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2024-01-16 17:08:50 +0300

    Tab Layout Setup

--- app/src/main/java/com/yusuf/cryptocurrencytrading/ui/mainScreens/MarketFragment.kt
+++ app/src/main/java/com/yusuf/cryptocurrencytrading/ui/mainScreens/MarketFragment.kt
@@ -5,8 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayoutMediator
 import com.yusuf.cryptocurrencytrading.R
 import com.yusuf.cryptocurrencytrading.databinding.FragmentMarketBinding
+import com.yusuf.cryptocurrencytrading.ui.topLossGain.adapter.TopLossGainAdapter
+import com.yusuf.cryptocurrencytrading.utils.gone
+import com.yusuf.cryptocurrencytrading.utils.visible
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -19,7 +24,40 @@ class MarketFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentMarketBinding.inflate(layoutInflater,container,false)
+
+        setTabLayout()
         return binding.root
+    }
+
+    private fun setTabLayout() {
+        val adapter = TopLossGainAdapter(this)
+        binding.contentViewPager.adapter = adapter
+
+        binding.contentViewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+
+                if (position == 0){
+                    binding.topGainIndicator.visible()
+                    binding.topLoseIndicator.gone()
+                }
+                else{
+                    binding.topGainIndicator.gone()
+                    binding.topLoseIndicator.visible()
+                }
+            }
+        })
+
+        TabLayoutMediator(binding.tabLayout,binding.contentViewPager){
+            tab, position ->
+            var title = if (position ==0){
+                "Top Gainers"
+            }
+            else{
+                "Top Losers"
+            }
+            tab.text = title
+        }.attach()
     }
 
 }
