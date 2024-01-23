d646228d25b2550b829c7aca75b81dcb0d57e269
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2024-01-23 18:05:16 +0300

    Add Balance Method Added & UserId and USerDocument Methods Updated

--- app/src/main/java/com/yusuf/cryptocurrencytrading/ui/mainScreens/viewModel/WalletViewModel.kt
+++ app/src/main/java/com/yusuf/cryptocurrencytrading/ui/mainScreens/viewModel/WalletViewModel.kt
@@ -1,10 +1,14 @@
 package com.yusuf.cryptocurrencytrading.ui.mainScreens.viewModel
 
+import android.content.Context
 import android.util.Log
+import android.view.View
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 import com.yusuf.cryptocurrencytrading.data.retrofit.repository.CoinRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -24,14 +28,14 @@ class WalletViewModel @Inject constructor(val repo: CoinRepository): ViewModel()
     fun getBalance() {
         viewModelScope.launch {
             try {
-                val userId = auth.currentUser?.uid ?: ""
-                val document = firestore.collection("users").document(userId).get().await()
 
-                Log.i("balance",document.getDouble("balance").toString())
+                if (getUserDocument() != null){
+                    Log.i("balance",getUserDocument()!!.getDouble("balance").toString())
 
-                balance.value = document.getDouble("balance")
-                Log.i("balanceValue",balance.value.toString())
+                    balance.value = getUserDocument()!!.getDouble("balance")
+                    Log.i("balanceValue",balance.value.toString())
 
+                }
 
             }
             catch (e:Exception){
@@ -39,4 +43,39 @@ class WalletViewModel @Inject constructor(val repo: CoinRepository): ViewModel()
             }
         }
     }
+
+    fun addToBalance(amountToAdd: Double,view: View) {
+        viewModelScope.launch {
+            try {
+
+                if (getUserDocument() != null){
+                    val currentBalance = getUserDocument()!!.getDouble("balance") ?: 0.0
+                    val newBalance = currentBalance + amountToAdd
+
+
+                    firestore.collection("users").document(getUserId())
+                        .update("balance", newBalance)
+                        .await()
+
+
+                    balance.value = newBalance
+
+                    Snackbar.make(view,"Adding Balance Successful",Snackbar.LENGTH_LONG).show()
+                }
+
+
+            } catch (e: Exception) {
+                Snackbar.make(view,"Adding Balance Not Successful",Snackbar.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun getUserId(): String{
+        return auth.currentUser?.uid ?: ""
+    }
+
+    private suspend fun getUserDocument(): DocumentSnapshot? {
+        return firestore.collection("users").document(getUserId()).get().await()
+    }
+
 }
