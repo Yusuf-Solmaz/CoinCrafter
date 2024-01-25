c32287beed87d5a26f1e2b6aa810c83beccfbba4
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2024-01-25 20:10:01 +0300

    Sell Buttons Completed and Connected With SellDialog

--- app/src/main/java/com/yusuf/cryptocurrencytrading/ui/mainScreens/adapter/WalletUserCryptoAdapter.kt
+++ app/src/main/java/com/yusuf/cryptocurrencytrading/ui/mainScreens/adapter/WalletUserCryptoAdapter.kt
@@ -1,29 +1,38 @@
 package com.yusuf.cryptocurrencytrading.ui.mainScreens.adapter
 
+import SellCryptoDialog
 import android.content.Context
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
 import com.yusuf.cryptocurrencytrading.R
 import com.yusuf.cryptocurrencytrading.data.firebase.entity.CryptoFirebase
 import com.yusuf.cryptocurrencytrading.data.retrofit.entity.CryptoCurrency
 import com.yusuf.cryptocurrencytrading.databinding.UserCryptoRowBinding
+import com.yusuf.cryptocurrencytrading.ui.mainScreens.viewModel.WalletViewModel
 
-class WalletUserCryptoAdapter(val context: Context,var userCoins: List<CryptoFirebase>,var coins:List<CryptoCurrency>): RecyclerView.Adapter<WalletUserCryptoAdapter.UserCryptoHolder>() {
+class WalletUserCryptoAdapter(val viewModel: WalletViewModel,val context: Context,var userCoins: List<CryptoFirebase>,var coins:List<CryptoCurrency>): RecyclerView.Adapter<WalletUserCryptoAdapter.UserCryptoHolder>() {
 
-    class UserCryptoHolder(val binding:UserCryptoRowBinding) : RecyclerView.ViewHolder(binding.root){
+    class UserCryptoHolder(val binding: UserCryptoRowBinding) :
+        RecyclerView.ViewHolder(binding.root) {
 
     }
+
     init {
-        Log.i("userCoinsSize",userCoins.size.toString())
-        Log.i("WalletAdapterInit",userCoins.toString())
+        Log.i("userCoinsSize", userCoins.size.toString())
+        Log.i("WalletAdapterInit", userCoins.toString())
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserCryptoHolder {
-        return UserCryptoHolder(UserCryptoRowBinding.inflate(LayoutInflater.from(parent.context),parent,false))
+        return UserCryptoHolder(
+            UserCryptoRowBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
     }
 
     override fun getItemCount(): Int {
@@ -41,13 +50,13 @@ class WalletUserCryptoAdapter(val context: Context,var userCoins: List<CryptoFir
         val id = (coinHashMap["id"] as Long).toInt()
 
         val coin = CryptoFirebase(amount, price, name, id)
-        Log.i("WalletAdapterCoin",userCoins.toString())
+        Log.i("WalletAdapterCoin", userCoins.toString())
 
         holder.binding.cardView.setBackgroundResource(R.drawable.coin_list_color)
         holder.binding.coinName.text = coin.name
 
-        Glide.with(context).load("https://s2.coinmarketcap.com/static/img/coins/64x64/${coin.id}.png")
-            .placeholder(R.drawable.baseline_currency_bitcoin_24).into(holder.binding.coinImg)
+
+        holder.binding.coinAmount.text = String.format("%.5f", coin.amount)
 
         for (data in coins) {
             if (coin.name != data.name) {
@@ -60,17 +69,52 @@ class WalletUserCryptoAdapter(val context: Context,var userCoins: List<CryptoFir
                 val profit = totalPrice - boughtCoinPrice
 
                 if (profit > 0) {
-                    holder.binding.coinProfit.setTextColor(ContextCompat.getColor(context, R.color.green))
-                    holder.binding.coinProfit.text = "${String.format("%.02f",profit)}$"
+                    holder.binding.coinProfit.setTextColor(
+                        ContextCompat.getColor(
+                            context,
+                            R.color.green
+                        )
+                    )
+                    holder.binding.coinProfit.text = "${String.format("%.02f", profit)}$"
                 } else if (profit < 0) {
-                    holder.binding.coinProfit.setTextColor(ContextCompat.getColor(context, R.color.red))
-                    holder.binding.coinProfit.text = "${String.format("%.02f",profit)}$"
+                    holder.binding.coinProfit.setTextColor(
+                        ContextCompat.getColor(
+                            context,
+                            R.color.red
+                        )
+                    )
+                    holder.binding.coinProfit.text = "${String.format("%.02f", profit)}$"
                 } else {
-                    holder.binding.coinProfit.setTextColor(ContextCompat.getColor(context, R.color.grey))
-                    holder.binding.coinProfit.text = "${String.format("%.02f",profit)}$"
+                    holder.binding.coinProfit.setTextColor(
+                        ContextCompat.getColor(
+                            context,
+                            R.color.grey
+                        )
+                    )
+                    holder.binding.coinProfit.text = "${String.format("%.02f", profit)}$"
 
                 }
             }
         }
+
+        holder.binding.buttonSell.setOnClickListener {
+
+            val sellDialog = SellCryptoDialog(context)
+            sellDialog.setOnSellClickListener(object : SellCryptoDialog.OnSellClickListener {
+                override fun onSellClick(sellAmount: Double) {
+
+                    viewModel.sellCrypto(coin.name, sellAmount,it,coins)
+                }
+            },object : SellCryptoDialog.OnSellAllClickListener{
+                override fun onSellAllClick() {
+
+                    viewModel.sellAllSelectedCrypto(coin.name,it,coins)
+                }
+            })
+            sellDialog.show()
+        }
     }
-}
+
+    }
+
+
