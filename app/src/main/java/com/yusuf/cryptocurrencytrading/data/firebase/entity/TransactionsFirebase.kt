e273d12ad8e80f1e2a785a9bb4f19fe199c93b1c
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2024-01-27 22:11:17 +0300

    Transaction Class Created

---
+++ app/src/main/java/com/yusuf/cryptocurrencytrading/data/firebase/entity/TransactionsFirebase.kt
@@ -0,0 +1,14 @@
+package com.yusuf.cryptocurrencytrading.data.firebase.entity
+
+import java.io.Serializable
+import java.util.Calendar
+import java.util.Date
+
+data class TransactionsFirebase (
+    var amountPrice: Double,
+    var amount: Double,
+    val status: String,
+    val name: String,
+    val id: Int,
+    val date: Date = Calendar.getInstance().time
+):Serializable
