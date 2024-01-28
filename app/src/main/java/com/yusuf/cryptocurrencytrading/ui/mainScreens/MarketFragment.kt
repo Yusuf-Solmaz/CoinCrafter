12f6e92b19ef0fd61f79b9f71d50a25ef23a7517
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2024-01-28 14:09:55 +0300

    Top Loss/Gain Updated

--- app/src/main/java/com/yusuf/cryptocurrencytrading/ui/mainScreens/MarketFragment.kt
+++ app/src/main/java/com/yusuf/cryptocurrencytrading/ui/mainScreens/MarketFragment.kt
@@ -26,10 +26,7 @@ class MarketFragment : Fragment() {
 
     private lateinit var binding: FragmentMarketBinding
     private lateinit var viewModel: MarketViewModel
-    private lateinit var adapter: MarketAdapter
-    private lateinit var searchText: String
 
-    private lateinit var list: List<CryptoCurrency>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,82 +35,16 @@ class MarketFragment : Fragment() {
         binding = FragmentMarketBinding.inflate(layoutInflater, container, false)
 
         setTabLayout()
-        list = listOf()
 
         viewModel = ViewModelProvider(this).get(MarketViewModel::class.java)
         viewModel.getAllCoins()
 
-
-        adapter = MarketAdapter(requireContext(), list)
-        binding.marketRecyclerView.adapter = adapter
-
         return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        setupRecyclerView()
-        observeCoins()
-        setupSearchView()
-    }
 
 
-    private fun setupSearchView() {
-        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener,
-            android.widget.SearchView.OnQueryTextListener {
 
-            override fun onQueryTextSubmit(query: String?): Boolean {
-                Log.i("submit", query.toString())
-
-                searchText = query.toString().toLowerCase()
-                updateRecyclerView(searchText)
-                return true
-            }
-
-            override fun onQueryTextChange(newText: String?): Boolean {
-                searchText = newText.toString().toLowerCase()
-                updateRecyclerView(searchText)
-                return true
-            }
-        })
-    }
-
-    private fun updateRecyclerView(searchText: String) {
-        Log.i("updateRecyclerView", "Search Text: $searchText")
-
-        val data = ArrayList<CryptoCurrency>()
-
-        for (item in list) {
-            val coinName = item.name.lowercase(Locale.getDefault())
-            val coinSymbol = item.symbol.lowercase(Locale.getDefault())
-
-            if (coinName.contains(searchText) || coinSymbol.contains(searchText)) {
-                Log.i("updateRecyclerViewListItem", item.name.toString())
-                data.add(item)
-            }
-        }
-
-        adapter.updateData(data)
-    }
-
-    private fun setupRecyclerView() {
-        val layoutManager = LinearLayoutManager(requireContext())
-        binding.marketRecyclerView.layoutManager = layoutManager
-    }
-
-    fun observeCoins() {
-        viewModel.coins.observe(viewLifecycleOwner) { coins ->
-            coins?.let {
-                if (list.isEmpty()) {
-
-                    list = coins.data.cryptoCurrencyList
-                    adapter = MarketAdapter(requireContext(), list)
-                    binding.marketRecyclerView.adapter = adapter
-                }
-            }
-        }
-    }
 
     private fun setTabLayout() {
         val adapter = TopLossGainAdapter(this)
