6105baae4acdf3692fd5952aa03dfa50e59187b5
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2024-01-29 23:22:41 +0300

    Crypto Favourites Class Created for Firebase

---
+++ app/src/main/java/com/yusuf/cryptocurrencytrading/data/firebase/entity/FavouriteCryptosFirebase.kt
@@ -0,0 +1,8 @@
+package com.yusuf.cryptocurrencytrading.data.firebase.entity
+
+import java.io.Serializable
+
+data class FavouriteCryptosFirebase (
+    val name: String,
+    val id: Int
+) : Serializable
